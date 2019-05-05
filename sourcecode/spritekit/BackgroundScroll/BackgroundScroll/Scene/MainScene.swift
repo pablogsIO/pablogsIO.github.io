f196d77c573a7daa897851462e8a68a64f547dc4
Pablo Garcia <pablo@pablogs.io>
2019-05-05 07:21:00 +0200

    swiftlint

--- sourcecode/spritekit/BackgroundScroll/BackgroundScroll/Scene/MainScene.swift
+++ sourcecode/spritekit/BackgroundScroll/BackgroundScroll/Scene/MainScene.swift
@@ -18,6 +18,38 @@ class MainScene: SKScene {
         
         super.init(size: size)
         
+        self.initFirstApproach()
+        
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func didMove(to view: SKView) {
+        print("pablogsio: \(#function)")
+    }
+
+}
+
+extension MainScene {
+    
+    private func drawLine(start: CGPoint, end: CGPoint, color: SKColor) {
+        
+        let line = SKShapeNode()
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: start.x, y: start.y))
+        path.addLine(to: CGPoint(x: end.x, y: end.y))
+        line.path = path
+        line.strokeColor = color
+        line.lineWidth = 2
+        addChild(line)
+        
+    }
+    
+    private func initFirstApproach() {
+
         var nodes = [SKSpriteNode]()
         let background1 = SKSpriteNode(imageNamed: "background_1")
         let background2 = SKSpriteNode(imageNamed: "background_2")
@@ -37,27 +69,5 @@ class MainScene: SKScene {
         drawLine(start: CGPoint(x: 0, y: self.frame.height/2), end: CGPoint(x: self.frame.width, y: self.frame.height/2),color: SKColor.blue)
         drawLine(start: CGPoint(x: self.frame.width/2, y: 0), end: CGPoint(x: self.frame.width/2, y: self.frame.height),color: SKColor.red)
         drawLine(start: CGPoint(x: self.frame.width/2+fakeScreenWidth, y: 0), end: CGPoint(x: self.frame.width/2+fakeScreenWidth, y: self.frame.height),color: SKColor.red)
-
     }
-    
-    private func drawLine(start: CGPoint, end: CGPoint, color: SKColor) {
-        
-        let line = SKShapeNode()
-        let path = CGMutablePath()
-        path.move(to: CGPoint(x: start.x, y: start.y))
-        path.addLine(to: CGPoint(x: end.x, y: end.y))
-        line.path = path
-        line.strokeColor = color
-        line.lineWidth = 2
-        addChild(line)
-        
-    }
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func didMove(to view: SKView) {
-        print("pablogsio: \(#function)")
-    }
-
 }
